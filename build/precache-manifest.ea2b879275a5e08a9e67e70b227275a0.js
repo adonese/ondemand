e4c9a4be9fe85aab60832f44e84c4d4b2968c5ce
Mohamed Yousif <mmbusif@gmail.com>
2020-10-09 12:23:56 +0200

    adding new dashboard build

---
+++ build/precache-manifest.ea2b879275a5e08a9e67e70b227275a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06080becd2e9b3177766a7b5201a41a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0536a3b66bd12283e404",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dd031e72491c5bac167a",
+    "url": "/static/js/2.7d78228a.chunk.js"
+  },
+  {
+    "revision": "b98d750e5d7aadb34528a4d84c1e4b1c",
+    "url": "/static/js/2.7d78228a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0536a3b66bd12283e404",
+    "url": "/static/js/main.0f4c26a6.chunk.js"
+  },
+  {
+    "revision": "84104a371422c7bfa96c",
+    "url": "/static/js/runtime-main.87d9142a.js"
+  }
+]);
