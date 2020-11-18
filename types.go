eb6c3ddfe894bda4781359145fc31f012eaa9580
Mohamed Yousif <mmbusif@gmail.com>
2020-11-18 15:33:02 +0200

    cleaning

--- types.go
+++ types.go
@@ -1543,9 +1543,7 @@ func (p *Provider) getProvidersWithScoreHandler(w http.ResponseWriter, r *http.R
 	sort.SliceStable(prov, func(i, j int) bool {
 		return prov[i].Haversine < prov[j].Haversine
 	})
-	// log.Printf("Haversine is: ID1:%d %f-%f", prov[0].ID, prov[0].Haversine, prov[1].Haversine)
 
-	log.Printf("The data after sorting is: %#v", data)
 	mData := make(map[string]interface{})
 	mData["result"] = prov
 	w.Header().Add("X-Total-Count", toString(len(prov)))
