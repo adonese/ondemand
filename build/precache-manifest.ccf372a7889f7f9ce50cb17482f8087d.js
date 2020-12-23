a983ea12b6c8afae9c9379abb14b105eafcf4d2c
Mohamed Yousif <mmbusif@gmail.com>
2020-12-23 21:20:18 +0200

    adding mobile checked

---
+++ build/precache-manifest.ccf372a7889f7f9ce50cb17482f8087d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89663c3fabc527c2d42bf83a698d3fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7c96cc7f9515f32cce0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa514451cffa2498ca74",
+    "url": "/static/js/2.de734f41.chunk.js"
+  },
+  {
+    "revision": "b350e126f260aced04b74f5e3f552f81",
+    "url": "/static/js/2.de734f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c96cc7f9515f32cce0",
+    "url": "/static/js/main.43f51e06.chunk.js"
+  },
+  {
+    "revision": "84104a371422c7bfa96c",
+    "url": "/static/js/runtime-main.87d9142a.js"
+  }
+]);
